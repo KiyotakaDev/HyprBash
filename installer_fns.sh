cf005e74a5cdd9b61967c60ff9096a90429fab9d
kiyota <c4mo.sw@gmail.com>
2024-03-14 15:31:44 -0500

    feat(is_pkg_installed): Adding fn to check if the package is installed

---
+++ installer_fns.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# NOTE: /dev/null is a special device that discards
+#       everything that is wirtten in avoding any output
+
+# Checks if the package is installed
+is_pkg_installed() {
+	local ChkPkg=$1
+	if pacman -Qq $ChkPkg &> /dev/null; then
+		# echo -e "\e[32m[$ChkPkg]\e[0m is installed"
+		return 0
+	else
+		# echo -e "\e[31m[$ChkPkg]\e[0m is NOT installed"
+		return 1
+	fi
+}

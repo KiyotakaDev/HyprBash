66ea6643f73172cf2a53a3cdd45ba29d8bc9f265
kiyota <c4mo.sw@gmail.com>
2024-03-15 16:54:23 -0500

    feat(text_format): Using global text formats

--- first_steps.sh
+++ first_steps.sh
@@ -7,7 +7,7 @@ source installer_fns.sh
 # Pacman configuration
 # If pacman conf exists and is does not have a back up; then
 if [ -f /etc/pacman.conf ] && [ ! -f /etc/pacman.conf.bak ]; then
-  echo -e "\e[33m[PACMAN]\e[0m making pacman prettier."
+  echo -e "${pacman_f} making pacman prettier."
   # Creates backup file
   sudo cp /etc/pacman.conf /etc/pacman.conf.bak
   
@@ -21,22 +21,22 @@ if [ -f /etc/pacman.conf ] && [ ! -f /etc/pacman.conf.bak ]; then
   /ParallelDownloads/a\ILoveCandy" /etc/pacman.conf
 
   sudo pacman -Syu
-  echo -e "\e[33m[PACMAN]\e[0m succesfully configured ^u^"
+  echo -e "${pacman_f} succesfully configured ^u^"
 else
-  echo -e "\e[1;47;30m[SKIP]\e[0m pacman is already configured"
+  echo -e "${skip_f} pacman is already configured"
 fi
 
 # Grub configuration
 # If grub instaled and grub.cfg file exists; then
 if is_pkg_installed grub && [ -f /boot/grub/grub.cfg ]; then
-  echo -e "\e[32m[BOOTLOADER]\e[0m GRUB detected."
+  echo -e "${bootldr_f} GRUB detected."
 
   if [ ! -f /etc/default/grub.bak ] && [ ! -f /boot/grub/grub.bak ]; then
-    echo -e "\e[32m[BOOTLOADER]\e[0m config starts."
+    echo -e "${bootldr_f} config starts."
     
     # Check nvidia
     if has_nvidia; then
-      echo -e "\e[32m[BOOTLOADER]\e[0m NVIDIA detected, adding nvidia_drm.modset=1 to boot option."
+      echo -e "${bootldr_f} NVIDIA detected, adding nvidia_drm.modset=1 to boot option."
       # 1: Set timeout to 15
       # 2: Replace ".*" to "nvidia_drm.modeset=1"
       sed -e '/^GRUB_TIMEOUT=/s/[0-9]\+$/15/' \
@@ -45,5 +45,5 @@ if is_pkg_installed grub && [ -f /boot/grub/grub.cfg ]; then
 
   fi
 else
-  echo -e "\e[1;47;30m[SKIP]\e[0m Grub is already configured"
+  echo -e "${skip_f} Grub is already configured"
 fi

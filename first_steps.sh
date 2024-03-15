b5293c34be609bb53f2fcf823a6a4d9bf3ceabce
kiyota <c4mo.sw@gmail.com>
2024-03-15 06:49:52 -0500

    feat(nvidia): Nvidia config is added

--- first_steps.sh
+++ first_steps.sh
@@ -23,15 +23,27 @@ if [ -f /etc/pacman.conf ] && [ ! -f /etc/pacman.conf.bak ]; then
   sudo pacman -Syu
   echo -e "\e[33m[PACMAN]\e[0m succesfully configured ^u^"
 else
-  echo -e "\e[7;37m[SKIP]\e[0m pacman is already configured"
+  echo -e "\e[1;47;30m[SKIP]\e[0m pacman is already configured"
 fi
 
 # Grub configuration
 # If grub instaled and grub.cfg file exists; then
 if is_pkg_installed grub && [ -f /boot/grub/grub.cfg ]; then
-  echo -e "\e[32m[GRUB]\e[0m detected."
+  echo -e "\e[32m[BOOTLOADER]\e[0m GRUB detected."
 
   if [ ! -f /etc/default/grub.bak ] && [ ! -f /boot/grub/grub.bak ]; then
-    echo -e "\e[32m[GRUB]\e[0m config starts."
+    echo -e "\e[32m[BOOTLOADER]\e[0m config starts."
+    
+    # Check nvidia
+    if has_nvidia; then
+      echo -e "\e[32m[BOOTLOADER]\e[0m NVIDIA detected, adding nvidia_drm.modset=1 to boot option."
+      # 1: Set timeout to 15
+      # 2: Replace ".*" to "nvidia_drm.modeset=1"
+      sed -e '/^GRUB_TIMEOUT=/s/[0-9]\+$/15/' \
+          -e '/^GRUB_CMDLINE_LINUX_DEFAULT=/s/".*"/"nvidia_drm.modeset=1"/' /etc/default/grub
+    fi
+
   fi
+else
+  echo -e "\e[1;47;30m[SKIP]\e[0m Grub is already configured"
 fi

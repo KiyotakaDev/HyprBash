dd49b164f6a9943fbff448881a5bd46610c11d7e
kiyota <c4mo.sw@gmail.com>
2024-03-15 17:37:17 -0500

    feat(checker): Base checker for packages

--- pkgs_installer.sh
+++ pkgs_installer.sh
@@ -3,10 +3,14 @@
 # Importing functions
 source installer_fns.sh
 
-
 pkgs_list=${1}
 
 while read -r pkg; do
-  if ! is_pkg_installed "$pkg"; then
+  if is_pkg_installed "$pkg"; then
+    echo -e "${skip_f} ${pkg} already installed."
+  elif is_pkg_available "$pkg"; then
+    echo -e "${pkg_f}\e[32m|${pkg}|\e[0m from ARCH official repo added to queue."
   else
-    echo -e "\e["
+    echo -e "${error_f} unknown ${pkg}."
+  fi
+done < $pkgs_list

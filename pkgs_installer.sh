a2aa28b7d699dc6acee7ae94edc8358db43e5847
kiyota <c4mo.sw@gmail.com>
2024-03-15 19:51:04 -0500

    feat(aur_installer): AUR installation is created

--- pkgs_installer.sh
+++ pkgs_installer.sh
@@ -3,6 +3,27 @@
 # Importing functions
 source installer_fns.sh
 
+# Check if user has AUR helper installed
+chk_aur
+# If arhlpr is is empty; then
+if [ -z $aurhlpr ] && [ ! is_pkg_installed yay && ! is_pkg_installed paru ]; then
+  echo -e "Select \e[32m|AUR|\e[0m helper:\n1) yay\n2) paru"
+  read -p "Enter number: " usrinp
+  
+  case $usrinp in
+    1) aurhlpr="yay";;
+    2) aurhlpr="paru";;
+    *) echo "Invalid option selected"
+       exit 1;;
+   esac
+   echo -e "${pkg_f}\e[32m|${aurhlpr}|\e[0m AUR helper is being installed"
+   # 2&>1 Shows both stdout and stderr shows (command output and error)
+   aur_installer $aurhlpr 2>&1
+ else
+   echo -e "${skip_f}\e[32m|$aurhlpr|\e[0m is already installed :D"
+fi
+
+# Recieves file as script option
 pkgs_list=${1}
 
 while read -r pkg; do

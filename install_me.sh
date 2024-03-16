4f2a872ef7509d851844edb9bd42f28a4403be04
kiyota <c4mo.sw@gmail.com>
2024-03-15 20:28:02 -0500

    feat(pkgs): Custom packages option is created

--- install_me.sh
+++ install_me.sh
@@ -28,4 +28,29 @@ cat << EOF
 
 EOF
 
-./first_steps.sh
+#./first_steps.sh
+
+# User file input
+custm_pkgs=$1
+# Preparing packages to install file
+cp def_hyprbsh_pkgs.txt pkgs_to_install.txt
+
+# If custm var is a regular file and is not empty; then
+if [ -f "$custm_pkgs" ] && [ -n "$cutm_pkgs" ]; then
+  cat $custm_pkgs >> pkgs_to_install.txt
+fi
+
+# Adding shell to list
+# If zsh and fish ar not installed; then
+if ! is_pkg_installed zsh && ! is_pkg_installed fish; then
+  echo -e "Select shell:\n1) zsh\n2) fish"
+  read -p "Enter number: " usrinp
+
+  case $usrinp in
+    1) shelly="zsh";;
+    2) shelly="fish";;
+    *) echo -e "Invalid option"
+       exit 1;;
+   esac
+   echo "$shelly" >> pkgs_to_install.txt
+fi

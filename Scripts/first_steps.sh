9078707a0c102cbce0087cc4195e8e0621f5a02d
kiyota <c4mo.sw@gmail.com>
2024-03-18 14:28:02 -0500

    feat(scripts): Moving all scripts to Scripts directory

---
+++ Scripts/first_steps.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+# Script for pre-install configuration (pacman and grup)
+# Importing functions
+source installer_fns.sh
+
+# Pacman configuration
+# If pacman conf exists and is does not have a back up; then
+if [ -f /etc/pacman.conf ] && [ ! -f /etc/pacman.conf.bak ]; then
+  echo -e "${pacman_f} making pacman prettier."
+  # Creates backup file
+  sudo cp /etc/pacman.conf /etc/pacman.conf.bak
+  
+  # 1: Uncomment Color
+  # 2: Uncomment ParallelDownloads
+  # 3: Uncomment VerbosePkgLists 
+  # 4: Append ILoveCandy bellow ParallelDownloads
+  sudo sed -i "/^#Color/s/^#//
+  /^#ParallelDownloads/s/^#//
+  /^#VerbosePkgLists/s/^#//
+  /ParallelDownloads/a\ILoveCandy" /etc/pacman.conf
+
+  sudo pacman -Syu
+  echo -e "${pacman_f} succesfully configured ^u^"
+else
+  echo -e "${skip_f} pacman is already configured"
+fi
+
+# Grub configuration
+# If grub instaled and grub.cfg file exists; then
+if is_pkg_installed grub && [ -f /boot/grub/grub.cfg ]; then
+  echo -e "${bootldr_f} GRUB detected."
+
+  if [ ! -f /etc/default/grub.bak ] && [ ! -f /boot/grub/grub.bak ]; then
+    echo -e "${bootldr_f} config starts."
+    
+    # Check nvidia
+    if has_nvidia; then
+      echo -e "${bootldr_f} NVIDIA detected, adding nvidia_drm.modset=1 to boot option."
+      # 1: Set timeout to 15
+      # 2: Replace ".*" to "nvidia_drm.modeset=1"
+      # sed -e '/^GRUB_TIMEOUT=/s/[0-9]\+$/15/' \
+      #     -e '/^GRUB_CMDLINE_LINUX_DEFAULT=/s/".*"/"nvidia_drm.modeset=1"/' /etc/default/grub
+    fi
+
+    # Custom theme
+    echo -e "Select grub theme:\n1) Void\n2) Sky\n3) Starfield"
+    read -p "Enter an option <or> Press enter to scape: " usrinp
+    case ${usrinp} in
+      1) theme="void";;
+      2) theme="sky";;
+      3) theme="starfield";;
+      *) theme=;;
+    esac
+
+    if [ -n "$theme" ]; then
+      echo -e "${bootldr_f}\e[36m|${theme}|\e[0m theme configuration :D"
+      sed -e "/^#GRUB_THEME/c\GRUB_THEME=\"/usr/share/grub/themes/${theme}/theme.txt\"" /etc/default/grub
+    else
+      echo -e "${skip_f} grub theme configuration."
+    fi
+
+  fi
+else
+  echo -e "${skip_f} Grub is already configured"
+fi
---
+++ Scripts/pkgs_installer.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Importing functions
+source installer_fns.sh
+
+# Check if user has AUR helper installed
+chk_aur
+# If arhlpr is is empty and yay and paru are not installed; then
+if [ -z $aurhlpr ] && [ ! is_pkg_installed yay && ! is_pkg_installed paru ]; then
+  echo -e "Select \e[32m|AUR|\e[0m helper:\n1) yay\n2) paru"
+  read -p "Enter number: " usrinp
+  
+  case $usrinp in
+    1) aurhlpr="yay";;
+    2) aurhlpr="paru";;
+    *) echo "Invalid option selected"
+       exit 1;;
+   esac
+   echo -e "${pkg_f}\e[32m|${aurhlpr}|\e[0m AUR helper is being installed"
+   # 2&>1 Shows both stdout and stderr shows (command output and error)
+   aur_installer $aurhlpr 2>&1
+ else
+   echo -e "${skip_f}\e[32m|$aurhlpr|\e[0m is already installed :D"
+fi
+
+# Recieves file as script option
+pkgs_list=${1}
+
+while read -r pkg; do
+  if is_pkg_installed "$pkg"; then
+    echo -e "${skip_f} ${pkg} already installed."
+  elif is_pkg_available "$pkg"; then
+    echo -e "${pkg_f}\e[32m|${pkg}|\e[0m from ARCH official repo added to queue."
+  else
+    echo -e "${error_f} unknown ${pkg}."
+  fi
+done < $pkgs_list

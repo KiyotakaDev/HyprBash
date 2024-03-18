48a546d65be91934674242765075874583fb4070
kiyota <c4mo.sw@gmail.com>
2024-03-18 16:33:26 -0500

    feat(completed): First steps is completed

--- Scripts/first_steps.sh
+++ Scripts/first_steps.sh
@@ -33,16 +33,19 @@ if is_pkg_installed grub && [ -f /boot/grub/grub.cfg ]; then
 
   if [ ! -f /etc/default/grub.bak ] && [ ! -f /boot/grub/grub.bak ]; then
     echo -e "${bootldr_f} config starts."
+    sudo cp /etc/default/grub /etc/default/grub.bak
+    sudo cp /boot/grub/grub.cfg /boot/grub/grub.cfg.bak
     
     # Check nvidia
     if has_nvidia; then
       echo -e "${bootldr_f} NVIDIA detected, adding nvidia_drm.modset=1 to boot option."
       # 1: Set timeout to 15
       # 2: Replace ".*" to "nvidia_drm.modeset=1"
-      # sed -e '/^GRUB_TIMEOUT=/s/[0-9]\+$/15/' \
-      #     -e '/^GRUB_CMDLINE_LINUX_DEFAULT=/s/".*"/"nvidia_drm.modeset=1"/' /etc/default/grub
+      sudo sed -i -e '/^GRUB_TIMEOUT=/s/[0-9]\+$/15/' \
+      -i -e '/^GRUB_CMDLINE_LINUX_DEFAULT=/s/".*"/"nvidia_drm.modeset=1"/' /etc/default/grub
     fi
 
+    echo -e "\e[36m[GRUB THEME]\e[0m"
     # Custom theme
     echo -e "Select grub theme:\n1) Void\n2) Sky\n3) Starfield"
     read -p "Enter an option <or> Press enter to scape: " usrinp
@@ -55,11 +58,20 @@ if is_pkg_installed grub && [ -f /boot/grub/grub.cfg ]; then
 
     if [ -n "$theme" ]; then
       echo -e "${bootldr_f}\e[36m|${theme}|\e[0m theme configuration :D"
-      sed -e "/^#GRUB_THEME/c\GRUB_THEME=\"/usr/share/grub/themes/${theme}/theme.txt\"" /etc/default/grub
+      ThmPth=${path}/Source/${theme}_theme.tar.gz
+      # Checks if the file exists
+      if [ -e $ThmPth ]; then
+        sudo tar -xzf ${ThmPth} -C /usr/share/grub/themes
+      fi
+      # Change the grub theme
+      sudo sed -i "/GRUB_GFXMODE/c\GRUB_GFXMODE=\1920x1080,1680x1050,auto
+      /^#GRUB_THEME/c\GRUB_THEME=\"/usr/share/grub/themes/${theme}_theme/theme.txt\"" /etc/default/grub
     else
       echo -e "${skip_f} grub theme configuration."
     fi
 
+    # Makes grub config file
+    sudo grub-mkconfig -o /boot/grub/grub.cfg
   fi
 else
   echo -e "${skip_f} Grub is already configured"

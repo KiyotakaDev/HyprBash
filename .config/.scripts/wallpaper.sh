53fcb91203b589aae7650be4037266aca605ed28
kiyota <c4mo.sw@gmail.com>
2024-03-22 12:43:41 -0500

    feat(structure): Next and prev functions

--- .config/.scripts/wallpaper.sh
+++ .config/.scripts/wallpaper.sh
@@ -6,3 +6,51 @@ cfg_fl="${thmPth}/.wallpaper_values.conf"
 # Themes
 themes=("anime" "neon" "lands")
 
+# Sourcing file
+if [ ! -f $cfg_fl ]; then
+  echo "saved_theme=0" > $cfg_fl
+  echo "saved_wallpaper=0" >> $cfg_fl
+  source $cfg_fl
+else
+  source $cfg_fl
+fi
+
+# Source variables
+crrnt_thm=$saved_theme
+crrnt_wllppr=$saved_wallpaper
+
+# Variables
+theme="${themes[$crrnt_thm]}"
+wallpapers=("$thmPth/$theme"/*)
+wallpaper="${wallpapers[$crrnt_thm]}"
+quantity=$((${#wallpapers[@]} - 1))
+
+# Functions
+show_wallpaper() {
+  local t_typ=${1:-"grow"}
+
+  swww img --transition-type $t_type \
+  --transition-pos 0.5,0.5 \
+  --transition-step 90 \
+  $wallpaper 
+}
+
+next_wallpaper() {
+  if [ $crrnt -lt  ]; then
+    (())
+}
+
+prev_wallpaper() {
+  if [ $crrnt -gt ]; then
+    (())
+}
+
+while getopts "np" opt; do
+  case $opt in
+    n) next_wallpaper;;
+    p) prev_wallpaper;;
+    *) echo "n: Next wallpaper"
+       echo "p: Previous wallpaper"
+       exit 1;;
+  esac
+done

6cf0cd1af0d7c6071d88054bb5166cac91a673b6
kiyota <c4mo.sw@gmail.com>
2024-03-22 13:12:29 -0500

    feat(wallpaper): Wallpaper shell comands is almost finished

--- .config/.scripts/wallpaper.sh
+++ .config/.scripts/wallpaper.sh
@@ -22,61 +22,6 @@ crrnt_wllppr=$saved_wallpaper
 # Variables
 theme="${themes[$crrnt_thm]}"
 wallpapers=("$thmPth/$theme"/*)
-wallpaper=${wallpapers[$crrnt_thm]}
-quantity=${#wallpapers[@]}
-
-# Functions
-save_values() {
-  echo "saved_theme=$crrnt_thm" > $cfg_fl
-  echo "saved_wallpaper=$crrnt_wllppr" >> $cfg_fl
-}
-
-show_wallpaper() {
-  local t_type=${1:-"grow"}
-
-  swww img --transition-type $t_type \
-  --transition-pos 0.5,0.5 \
-  --transition-step 90 \
-  $wallpaper 
-}
-
-next_wallpaper() {
-  if [ $crrnt_wllppr -le $((quantity - 1)) ]; then
-    ((crrnt_wllppr++))
-  else
-    crrnt_wllppr=0
-  fi
-  show_wallpaper
-}
-
-prev_wallpaper() {
-  if [ $crrnt_wllppr -gt 0 ]; then
-    ((crrnt_wllppr--))
-[kiyota@mirai .scripts]$ cat < wallpaper.sh 
-# Path
-srcPth=`dirname "$(dirname "$(realpath "$0")")"`
-thmPth="${srcPth}/swww"
-cfg_fl="${thmPth}/.wallpaper_values.conf"
-
-# Themes
-themes=("anime" "neon" "lands")
-
-# Sourcing file
-if [ ! -f $cfg_fl ]; then
-  echo "saved_theme=0" > $cfg_fl
-  echo "saved_wallpaper=0" >> $cfg_fl
-  source $cfg_fl
-else
-  source $cfg_fl
-fi
-
-# Source variables
-crrnt_thm=$saved_theme
-crrnt_wllppr=$saved_wallpaper
-
-# Variables
-theme="${themes[$crrnt_thm]}"
-wallpapers=("$thmPth/$theme"/*)
 wallpaper=${wallpapers[$crrnt_wllppr]}
 quantity=${#wallpapers[@]}
 

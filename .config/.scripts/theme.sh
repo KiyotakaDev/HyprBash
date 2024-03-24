ca788a887e4b3dc03ddd51b965c5c3e65fb860e7
kiyota <c4mo.sw@gmail.com>
2024-03-23 19:37:19 -0500

    feat(theme): Theme change func is added

---
+++ .config/.scripts/theme.sh
@@ -0,0 +1,38 @@
+# Sourcing global variables
+source global.sh
+
+# Functions
+print_theme() {
+  echo ID: $curr_theme
+  echo Theme: $theme
+}
+
+next_theme() {
+  # print_theme
+  if [ $curr_theme -lt $theme_quantity ]; then
+    ((curr_theme++))
+  else
+    curr_theme=0
+  fi
+}
+
+prev_theme() {
+  # print_theme
+  if [ $curr_theme -gt 0 ]; then
+    ((curr_theme--))
+  else
+    curr_theme=$((theme_quantity))
+  fi
+}
+
+while getopts "np" opt; do
+	case $opt in
+		n) next_theme;;
+		p) prev_theme;;
+		*) echo "n: Next theme"
+		   echo "p: Previous theme"
+		   exit 1;;
+	esac
+done
+
+save_values

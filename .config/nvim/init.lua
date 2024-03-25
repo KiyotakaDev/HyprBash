9fdc3887d8040a3447de6bed69e78e2e34f66f92
kiyota <c4mo.sw@gmail.com>
2024-03-25 06:34:29 -0500

    feat(nvim): Nvim configuration base file

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,4 +1,19 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
+-- Vim config
+require("vim")
+
+-- Lazy.vim installation
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Plugins settup
+require("lazy").setup("plugins")

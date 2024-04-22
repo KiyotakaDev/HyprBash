8d4617dc5f9216bd60375c068c00bff76e2c8063
kiyota <c4mo.sw@gmail.com>
2024-04-22 16:34:26 -0500

    feat(javascript): treesitter

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
   config = function()
     local config = require('nvim-treesitter.configs')
     config.setup({
-      ensure_installed = { 'bash', 'lua', 'c', 'cpp', 'glsl' },
+      ensure_installed = { 'bash', 'lua', 'c', 'cpp', 'glsl', 'javascript' },
       highlight = { enable = true },
       indent = { enable = true },
     })

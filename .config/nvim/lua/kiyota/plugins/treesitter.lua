eb2cb9adcb864eeee757d66650383becc08f8675
kiyota <c4mo.sw@gmail.com>
2024-05-07 07:52:02 -0500

    feat(order): Sorting

--- .config/nvim/lua/kiyota/plugins/treesitter.lua
+++ .config/nvim/lua/kiyota/plugins/treesitter.lua
@@ -11,16 +11,6 @@ return {
 
     -- configure treesitter
     treesitter.setup({ -- enable syntax highlighting
-      highlight = {
-        enable = true,
-      },
-      -- enable indentation
-      indent = { enable = true },
-      -- enable autotagging (w/ nvim-ts-autotag plugin)
-      autotag = {
-        enable = true,
-      },
-      -- ensure these language parsers are installed
       ensure_installed = {
         "bash",
         "vim",
@@ -31,7 +21,17 @@ return {
         "css",
         "gitignore",
       },
-      incremental_selection = {
+       highlight = {
+        enable = true,
+      },
+      -- enable indentation
+      indent = { enable = true },
+      -- enable autotagging (w/ nvim-ts-autotag plugin)
+      autotag = {
+        enable = true,
+      },
+      -- ensure these language parsers are installed
+     incremental_selection = {
         enable = true,
         keymaps = {
           init_selection = "<C-space>",

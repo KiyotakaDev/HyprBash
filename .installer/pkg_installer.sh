e5cbdcf5c580f6cbe9c51cea5e5bb4070c810774
kiyota <c4mo.sw@gmail.com>
2024-05-19 08:44:57 -0500

    feat(sourcing): Package functions

--- .installer/pkg_installer.sh
+++ .installer/pkg_installer.sh
@@ -1,24 +1,21 @@
 #!/bin/bash
 
+# Sourcing package functions
+source ./pkg_functions.sh
+if [ $? -eq 1 ]; then
+  echo "Error sourcing file"
+  exit 1
+fi
+
 # Takes spaces as delimiter
 packages=($(awk '{print $1}' ./default_packages.txt))
 
-is_pkg_installed() {
-  local pkg_to_check=$1
 
-  if pacman -Qq "$pkg_to_check" &> /dev/null; then
-    return 0 # Installed
-  else
-    return 1 # Not installed
-  fi
-}
+# Packages installation
+yay_installer
 
-for pkg in "${packages[@]}"; do
-  if is_pkg_installed "$pkg"; then
-    echo "SKIP $pkg"
-  else
-    echo "PKG $pkg queued"
-  fi
+for pkg in "${packages[1]}"; do
+  echo "Hello"
 done
 
 # TODO: export XDG_CONFIG_HOME="$HOME/HyprBash/.config"

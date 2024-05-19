dadff65277101581b2a5fad476d3b5142f53d245
kiyota <c4mo.sw@gmail.com>
2024-05-19 09:56:42 -0500

    feat(text): Formats are added

---
+++ .installer/formats.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Text formats
+skip_f="\e[1;47;30m[SKIP]\e[0m"
+pacman_f="\e[33m[PACMAN]\e[0m"
+boot_f="\e[32m[BOOTLOADER]\e[0m"
+pkg_f="\e[36m[PACKAGE]\e[0m"
+display_f="\e[45;37m[DISPLAYMANAGER]\e[0m"
+error_f="\e[31m[ERROR]\e[0m"

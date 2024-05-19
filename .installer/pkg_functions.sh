658606915e21471b211920e3730ae91ab0bcb6ac
kiyota <c4mo.sw@gmail.com>
2024-05-19 09:57:10 -0500

    feat(formats): Are used

--- .installer/pkg_functions.sh
+++ .installer/pkg_functions.sh
@@ -1,10 +1,19 @@
+#!/bin/bash
+
+# Sourcing formats
+source ./formats.sh
+if [ $? -eq 1 ]; then
+  echo "Error sourcing formats"
+fi
+
+
 is_pkg_installed() {
   local pkg_to_check=$1
 
   if pacman -Qq "$pkg_to_check" &> /dev/null; then
-    return 0 # Installed
+    echo -e "$skip_f |$pkg_to_check| is already installed >.<"
   else
-    return 1 # Not installed
+    echo -e "$pkg_f |$pkg_to_check| is not installed UnU"
   fi
 }
 
@@ -12,16 +21,31 @@ is_arch_available() {
   local pkg_to_check=$1
 
   if pacman -Ss "$pkg_to_check" &> /dev/null; then
-    return 0 # On arch repo
+    echo -e "$pkg_f |$pkg_to_check| is on Arch repo"  
   else
-    return 1 # Not on arch repo
+    echo -e "$pkg_f |$pkg_to_check| is not on Arch repo"
+  fi
+}
+is_yay_available() {
+  local pkg_to_check=$1
+
+  if yay -Ss "$pkg_to_check" &> /dev/null; then
+    echo "Hello"
+  else
+    echo "Bye"
   fi
 }
 
 yay_installer() {
   if ! is_pkg_installed "git"; then
     echo "PKG git must be installed"
+    sudo pacman -S git
   else
-    echo "Not installed"
+    echo "SKIP git is already installed"
   fi
+
+  # echo "PKG yay installation"
+  # git clone https://aur.archlinux.org/yay.git
+  # cd yay
+  # makepkg -si
 }

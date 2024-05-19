6ae928f99b652f12b3034e568f53b6bc4fa8e1b9
kiyota <c4mo.sw@gmail.com>
2024-05-19 11:24:43 -0500

    feat(pkg): Installer functionality is added

--- .installer/pkg_installer.sh
+++ .installer/pkg_installer.sh
@@ -15,6 +15,7 @@ source ./formats.sh || handle_error "Sourcing formats file"
 packages=($(grep -E '^[^#]+[[:space:]]+#?' default_packages.txt | awk '{print $1}'))
 
 # YAY helper installer
+echo -e "\n\e[36m[INSTALLING HELPER ╰(▔∀▔)╯]\e[0m"
 yay_installer
 
 # Arrays to store package list
@@ -22,14 +23,30 @@ arch_pkgs=()
 yay_pkgs=()
 error_pkgs=()
 
+echo -e "\n\e[36m[LISTING PACKAGES (￣▽￣)]\e[0m"
 for pkg in "${packages[@]}"; do
-  if ! is_arch_available "$pkg" && ! is_yay_available "$pkg"; then
+  if is_pkg_installed "$pkg"; then
+    echo -e "${skip_f}\e[32m|$pkg|\e[0m is already installed."
+  elif ! is_arch_available "$pkg" && ! is_yay_available "$pkg"; then
     error_pkgs+=("$pkg")
   elif is_arch_available "$pkg"; then
+    echo -e "${pkg_f}\e[32m|$pkg|\e[0m from ARCH repo queued"
     arch_pkgs+=("$pkg")
   elif is_yay_available "$pkg"; then
+    echo -e "${pkg_f}\e[32m|$pkg|\e[0m from YAY repo queued"
     yay_pkgs+=("$pkg")
   fi  
 done
+
+echo -e "\n\e[36m[INSTALLING PACKAGES o(≧▽≦)o]\e[0m"
+if [ "${#arch_pkgs[@]}" -gt 0 ]; then
+  sudo pacman -S "${arch_pkgs[@]}"
+fi
+if [ "${#yay_pkgs[@]}" -gt 0 ]; then
+  yay -S "${yay_pkgs[@]}"
+fi
+if [ "${#error_pkgs[@]}" ]; then
+  echo -e "${error_f}\e[32m|$pkg|\e[0m not found"
+fi
 
 # TODO: export XDG_CONFIG_HOME="$HOME/HyprBash/.config"

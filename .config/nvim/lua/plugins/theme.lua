9c68da70e0670d8ceac564c2d2b427246dc23aa4
kiyota <c4mo.sw@gmail.com>
2024-05-21 12:29:38 -0500

    feat(theme): Is changed

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -1,7 +1,15 @@
+-- return {
+--  "folke/tokyonight.nvim",
+--  priority = 1000,
+--  config = function()
+--    vim.cmd("colorscheme tokyonight-night")
+--  end
+-- }
 return {
-  "folke/tokyonight.nvim",
+  "catppuccin/nvim",
+  name = "catppuccin",
   priority = 1000,
   config = function()
-    vim.cmd("colorscheme tokyonight-night")
+    vim.cmd("colorscheme catppuccin")
   end
 }

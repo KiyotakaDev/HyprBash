c31f907d2b1370e1e414696cf0ecba12382269ea
kiyotaka <c4mo.sw@gmail.com>
2024-07-16 16:32:32 -0500

    feat(bash): Scripts are added to the executable

--- .installer/execute_me.sh
+++ .installer/execute_me.sh
@@ -10,8 +10,9 @@ source ./pkg_functions.sh || handle_error "Sourcing functions file"
 source ./formats.sh || handle_error "Sourcing formats file"
 
 # Packages installer
-./pkg_installer.sh ./default_packages.txt
+# ./pkg_installer.sh ./default_packages.txt
 
+# Symbolic link for ~/HyprBash/.config/* files
 if [ ! -d ~/.config-bak ]; then
   echo -e "\e[36m[SYMLINK GENERATION]\e[0m"
   mv ~/.config ~/.config-bak
@@ -21,23 +22,8 @@ else
   echo -e "${skip_f}\e[36m[SYMLINK]\e[0m"
 fi
 
-if has_nvidia; then
-  echo -e "\e[32m[NVIDIA CONFIGURATION]\e[0m"
-  mkinit=/etc/mkinitcpio.conf
-  if [ ! -f "${mkinit}.bak" ]; then
-    sudo cp $mkinit "${mkinit}.bak"
-    sudo sed -i '/^MODULES=/c\MODULES=(nvidia nvidia_modeset nvidia_uvm nvidia_drm)' $mkinit
-  else
-    echo -e "${skip_f} mkinitcpio.conf is already configured"
-  fi
+# NVIDIA configuration
+./nvidia_conf.sh
 
-  modfile=/etc/modprobe.d/nvidia.conf
-  if [ ! -f $modfile ] && [ ! -f "${modfile}.bak" ]; then
-    echo "options nvidia_drm modeset=1 fbdev=1" | sudo tee $modfile &> /dev/null
-  else
-    echo -e "${skip_f} modprobe.d is already configured"
-  fi
-
-  sudo mkinitcpio --config /etc/mkinitcpio.conf --generate /boot/initramfs-custom.img
-  sed -i '/^#source/c\source=$src/hypr/env.conf            # ENV variables' ~/HyprBash/.config/hypr/hyprland.conf
-fi
+# Base configuration (pacman & grub)
+./base_conf.sh

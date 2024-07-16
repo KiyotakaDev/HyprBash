7dbf86bd7ab72bbc29e8191337fed513b6f025c1
kiyotaka <c4mo.sw@gmail.com>
2024-07-16 16:30:52 -0500

    feat(grub): Configuration script

---
+++ .installer/base_conf.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# Function to handle source error
+handle_error() {
+  echo -e "\e[31m[ERROR]\e[0m $1"
+  exit 1
+}
+# Sourcing files
+source ./pkg_functions.sh || handle_error "Sourcing functions file" 
+source ./formats.sh || handle_error "Sourcing formats file"
+
+# Pacman configuration
+if [ -f /etc/pacman.conf ] && [ ! -f /etc/pacman.conf.bak ]; then
+  echo -e "${pacman_f} configuration"
+  # Backup file
+  sudo cp /etc/pacman.conf /etc/pacman.conf.bak
+
+  # Color, ParallelDownloads, VerbosePkgLists and ILoveCandy
+  sudo sed -i "/^#Color/s/^#//
+  /^#ParallelDownloads/s/^#//
+  /^#VerbosePkgLists/s/^#//
+  /ParallelDownloads/a\ILoveCandy" /etc/pacman.conf
+
+  sudo pacman -Syu
+  echo -e "${pacman_f} succesfully configured ^u^"
+else
+  echo -e "${skip_f} pacman already configured..."
+fi
+
+# Grub configuration
+if [ ! -f /etc/default/grub.bak ] && [ ! -f /boot/grub/grub.cfg.bak ]; then
+  echo -e "${boot_f} configuration"
+  # Backup
+  sudo cp /etc/default/grub /etc/default/grub.bak
+  sudo cp /boot/grub/grub.cfg /boot/grub/grub.cfg.bak
+
+  if has_nvidia; then
+    echo -e "${boot_f} NVIDIA detected, adding \"nvidia_drm.modset=1\" to boot option"
+    sudo sed -i -e '/^GRUB_TIMEOUT=/s/[0-9]\+$/15/' \
+      -i -e '/^GRUB_CMDLINE_LINUX_DEFAULT=/s/".*"/"nvidia_drm.modeset=1"/' /etc/default/grub
+  fi
+
+  # THEME
+  echo -e "\e[36m[GRUB THEME]\e[0m"
+  themePath=~/HyprBash/assests/grub/void_theme.tar.gz
+  if [ -e $themePath ]; then
+    sudo tar -xzf ${themePath} -C /usr/share/grub/themes
+    # Change THEME
+    sudo sed -i "/GRUB_GFXMODE/c\GRUB_GFXMODE=\1680x1050,auto,auto
+    /^#GRUB_THEME/c\GRUB_THEME=\"/usr/share/grub/themes/void_theme/theme.txt\"" /etc/default/grub
+    # Make grub config file
+    sudo grub-mkconfig -o /boot/grub/grub.cfg
+  fi
+else
+  echo -e "${skip_f} GRUB is already configured"
+fi

5a904687cda8a838d79299121f916cd7a03bcda5
kiyotaka <c4mo.sw@gmail.com>
2024-09-20 11:19:45 -0500

    feat(state): variables now are global

---
+++ .scripts/state.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+STATE_FILE="$HOME/HyprBash/.scripts/.wallpaper_state"
+
+if [ ! -f "$STATE_FILE" ]; then
+  echo "abstract,1" > "$STATE_FILE"
+fi
+
+# Variables
+current_theme=
+current_wallpaper=
+WALL_QUANTITY=
+
+set_current() {
+  # Internal Field Separator (abstract,1) => abstract 1
+  IFS=',' read current_theme current_wallpaper < "$STATE_FILE"
+}
+
+# Gets the amount of images in the directory
+get_wall_quantity() {
+  local theme_dir="$HOME/HyprBash/assests/swww/$current_theme"
+
+  if [ -d "$theme_dir" ]; then
+    WALL_QUANTITY=$(ls "$theme_dir" | wc -l)
+  else
+    WALL_QUANTITY=1
+  fi
+}

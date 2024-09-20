b0d728dea557f6c43377584e0bb920fe74aaf7ca
kiyotaka <c4mo.sw@gmail.com>
2024-09-20 11:29:47 -0500

    feat(load): Wallpaper function

--- .scripts/main.sh
+++ .scripts/main.sh
@@ -10,13 +10,22 @@ source "$DIR/theme.sh"
 set_current
 
 set_wallpaper() {
-  theme="$1"
-  wallpaper="$2"
-  animation=${3:-"wipe"}
-  wallpaper_path="$HOME/HyprBash/assests/swww/$1/wall_$2.png"
+  local theme="$1"
+  local wallpaper="$2"
+  local animation=${3:-"wipe"}
+  local wallpaper_path="$HOME/HyprBash/assests/swww/$theme/wall_$wallpaper.png"
 
   swww img --transition-type "$animation" \
            --transition-pos 0.5,0.5 \
+           --transition-step 20 \
+           --transition-fps 165 \
+           "$wallpaper_path"
+}
+load_wallpaper() {
+  local wallpaper_path="$HOME/HyprBash/assests/swww/$current_theme/wall_$current_wallpaper.png"
+
+  swww img --transition-type wipe \
+           --transition-angle 180 \
            --transition-step 20 \
            --transition-fps 165 \
            "$wallpaper_path"
@@ -26,7 +35,7 @@ while getopts "npcud" opt; do
   case "$opt" in
     n) change_wallpaper "next" "grow";;
     p) change_wallpaper "prev" "outer";;
-    c) set_wallpaper "$current_theme" "$current_wallpaper";;
+    c) load_wallpaper;;
     u) change_theme "next";;
     d) change_theme "prev";;
     *) echo "n: Next wallpaper"

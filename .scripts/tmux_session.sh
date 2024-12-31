4fa13eb521ac2439e8f7e5006a70b249c888a6cd
Kiyota <c4mo.sw@gmail.com>
2024-12-30 20:34:24 -0500

    update(tmux_session)

--- .scripts/tmux_session.sh
+++ .scripts/tmux_session.sh
@@ -2,18 +2,16 @@
 
 create_session() {
   local session_name=$1
-  local initial_command=$2
 
   # Checks if session exists
   if ! tmux has-session -t "$session_name" &> /dev/null; then
     # If not create session
-    tmux new-session -d -s "$session_name" "${initial_command}; bash"
+    tmux new-session -d -s "$session_name" "fastfetch; bash"
   fi
 }
 
 # Create sessions
-create_session "MAIN" "echo 'Welcome to Main session'"
-create_session "2ND" "echo 'Welcome to 2ND session'"
+create_session "MAIN"
 
 # Open kitty and attach tmux session
 kitty tmux attach-session -t "MAIN"

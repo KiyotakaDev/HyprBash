6a297ec1a5bc8bd3662bc920d1fba35921380500
Kiyota <c4mo.sw@gmail.com>
2025-02-05 17:24:41 -0500

    feat(flags): Now execute_me script uses flags for pkgs installation

--- .installer/execute_me.sh
+++ .installer/execute_me.sh
@@ -13,7 +13,28 @@ source ./formats.sh || handle_error "Sourcing formats file"
 ./base_conf.sh
 
 # 2: Packages installer script
-./pkg_installer.sh
+if [[ $# -eq 0 ]]; then
+  echo "This command needs a flag to be executed"
+  echo "Execute with -h to know the flags"
+  exit 1
+fi
+
+while getopts "dsch" opt; do
+  case "$opt" in
+    d) ./pkg_installer.sh ./dev_packages.txt;;
+    s) ./pkg_installer.sh;;
+    c) ./pkg_installer.sh $2;;
+    h) echo -e "\t-d\tWith developer packages"
+       echo -e "\t-s\tOnly system needed packages"
+       echo -e "\t-c\tWith custom packages (cmd -c file/path/file.txt)"
+       echo -e "\t-h\tList command flags"
+       exit 1
+      ;;
+    *) echo "Command needs a flag. Execute with -h flag for more info"
+       exit 1
+       ;;
+  esac
+done
 
 # 3: NVIDIA specific config script 
 ./nvidia_conf.sh
